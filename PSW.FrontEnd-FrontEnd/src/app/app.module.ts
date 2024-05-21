1642f5594b11d9b6926df8677d234c7d43c58689
z1 <miljanlazic@gmail.com>
2024-05-21 17:10:31 +0200

    FEAT: Navbar, footer, login and register mockup

--- PSW.FrontEnd-FrontEnd/src/app/app.module.ts
+++ PSW.FrontEnd-FrontEnd/src/app/app.module.ts
@@ -7,9 +7,11 @@ import { AppComponent } from './app.component';
 import { AuthorModule } from '../components/Author/author.module';
 import { TouristModule } from '../components/Tourist/tourist.module';
 import { UniversalModule } from '../components/Universal/universal.module';
+import { NavbarComponent } from '../components/Universal/navbar/navbar.component';
+import { FooterComponent } from '../components/Universal/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent, FooterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- PSW.FrontEnd-FrontEnd/src/components/Universal/register/register.component.ts
+++ PSW.FrontEnd-FrontEnd/src/components/Universal/register/register.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  constructor(private router: Router) {}
 
+  navigateToLogin(): void {
+    this.router.navigate(['/login']);
+  }
 }
