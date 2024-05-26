6904509b29cdd0ac0c7b631d3776a1af50300909
z1 <miljanlazic@gmail.com>
2024-05-27 00:20:21 +0200

    FIX: Router Bugfix

--- PSW.FrontEnd-FrontEnd/src/components/Universal/universal.module.ts
+++ PSW.FrontEnd-FrontEnd/src/components/Universal/universal.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { HomePageComponent } from './home-page/home-page.component';
 import { ErrorComponent } from './error/error.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HomePageComponent, ErrorComponent],
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   exports: [HomePageComponent],
 })
 export class UniversalModule {}
